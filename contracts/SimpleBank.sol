8d68029642a13b10df6a3d1fc38018d0d67975e1
paltrickontpb <paltrickontpb@gmail.com>
2019-07-31 02:55:07 +0530

    Address Indexed

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -26,14 +26,14 @@ contract SimpleBank {
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled(address accountAddress);
+    event LogEnrolled(address indexed accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade(address accountAddress, uint amt);
+    event LogDepositMade(address indexed accountAddress, uint amt);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
+    event LogWithdrawal(address indexed accountAddress, uint withdrawAmount, uint newBalance);
 
     //
     // Functions
